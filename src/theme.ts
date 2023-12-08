c8be5af1b4b476d61eb02fd3776b3de46fcb5127
phupha <phupha@gopomelo.com>
2023-12-08 16:45:35 +0700

    Add theme configuration file

---
+++ src/theme.ts
@@ -0,0 +1,53 @@
+const theme = {
+  colors: {
+    base: {
+      base: "#fff",
+      baseContrast: "#F4F5F7",
+    },
+    greyscale: {
+      "100": "#192434",
+      "90": "#263445",
+      "80": "#424D5F",
+      "70": "#626D7C",
+      "60": "#77828F",
+      "50": "#8993A1",
+      "40": "#AAB1BC",
+      "30": "#C4CAD3",
+      "20": "#DBDFE5",
+      "10": "#E7EBEF",
+      "5": "#F4F5F7",
+    },
+    text: {
+      heading: "#192434",
+      body: "#273444",
+      secondary: "#626D7C",
+      terinary: "#8993A1",
+      disabled: "#C4CAD3",
+    },
+    primary: {
+      text: "#015FDB",
+      "100": "#016BF6",
+      "50": "#3D8FFB",
+      "10": "#E4F0FF",
+    },
+    error: {
+      text: "#E31C12",
+      "100": "#CB160B",
+      "50": "#F38E89",
+      "10": "#FEE8E8",
+    },
+    warning: {
+      text: "#985211",
+      "100": "#FFCC02",
+      "10": "#FFEECC",
+    },
+    sucess: {
+      text: "#097637",
+      "100": "#139C4C",
+      "50": "#8AC3A2",
+      "10": "#E8F3ED",
+    },
+  },
+};
+
+export default theme;
