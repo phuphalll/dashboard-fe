35e2f418f7f5b90a5e3c86399901be670699168d
phupha <phupha@gopomelo.com>
2023-12-08 17:17:33 +0700

    Add Dashboard route

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Dashboard from "./pages/Dashboard";
+import Providers from "./provider/Provider";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename="/">
+      <Providers>
+        <AppContainer>
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/" element={<div>test</div>} />
+          </Routes>
+        </AppContainer>
+      </Providers>
+    </Router>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
