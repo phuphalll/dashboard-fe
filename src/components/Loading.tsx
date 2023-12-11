207af06b7ac2456921d362d3df8de69e2644e6c8
phupha <phupha@gopomelo.com>
2023-12-11 13:59:10 +0700

    Add number-condition function

---
+++ src/components/Loading.tsx
@@ -0,0 +1,36 @@
+import styled, { keyframes } from "styled-components";
+
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+const Spinner = styled.div`
+  border: 4px solid rgba(255, 255, 255, 0.3);
+  border-radius: 50%;
+  border-top: 4px solid white;
+  width: 50px;
+  height: 50px;
+  animation: ${spin} 1s linear infinite;
+`;
+
+const LoadingSpinner = () => (
+  <Overlay>
+    <Spinner />
+  </Overlay>
+);
+
+export default LoadingSpinner;
